f41605e85dee5966a1d04073031b635e6802ec4c
Alexander Zhukov <zuav@crystax.net>
2015-11-11 13:00:06 +0300

    Simplify crew scripts (both unix and win versions)

--- crew.cmd
+++ crew.cmd
@@ -8,29 +8,27 @@ set GEM_PATH=
 set CREWFILEDIR=%~dp0
 set CREWHOSTOS=windows
 
+if not defined SSL_CERT_FILE (
+    set SSL_CERT_FILE=%CREWFILEDIR%etc\ca-certificates.crt
+)
+
+if not defined CREW_NDK_DIR (
+    set CREW_NDK_DIR=%CREWFILEDIR%..\..
+)
+
 set CREWHOSTCPU=-x86_64
-if not exist %CREWFILEDIR%..\..\prebuilt\windows%CREWHOSTCPU% (
+if not exist %CREW_NDK_DIR%\prebuilt\windows%CREWHOSTCPU% (
    set CREWHOSTCPU=
 )
 
-if not defined SSL_CERT_FILE (
-    set SSL_CERT_FILE=%CREWFILEDIR%\etc\ca-certificates.crt
-)
-
 if not defined CREW_TOOLS_DIR (
-   set CREW_TOOLS_DIR=%CREWFILEDIR%..\..\prebuilt\windows%CREWHOSTCPU%
+   set CREW_TOOLS_DIR=%CREW_NDK_DIR%\prebuilt\windows%CREWHOSTCPU%
 )
 
 rem set CREW
 rem set GIT
 
-set CREWRUBYVER=
-pushd %CREW_TOOLS_DIR%\crew\ruby
-for /f "delims=" %%a in ('type active_version.txt') do @set CREWRUBYVER=%%a
-popd
-set CREWRUBYDIR=%CREW_TOOLS_DIR%\crew\ruby\%CREWRUBYVER%\bin
-
-%CREWRUBYDIR%\ruby.exe -W0 %CREWFILEDIR%crew.rb %*
+call %CREW_TOOLS_DIR%\bin\ruby.cmd -W0 %CREWFILEDIR%crew.rb %*
 
 endlocal
 
