7b6ec020ddb205d6cacddc0b2de61d43e8db628f
Alexander Zhukov <zuav@crystax.net>
2015-12-22 22:22:55 +0300

    Install icu4c sources

--- formula/icu4c.rb
+++ formula/icu4c.rb
@@ -2,6 +2,7 @@ class Icu4c < Library
 
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "http://site.icu-project.org/"
+  url "https://ssl.icu-project.org/files/icu4c/${version}/icu4c-${block}-src.tgz" do |v| v.gsub('.', '_') end
 
-  release version: '55.1', crystax_version: 1, sha256: '32b361243add65971e35a9712f69a8fb373c5a33c66e0459895dc1f39f562ff2'
+  release version: '56.1', crystax_version: 1, sha256: '0'
 end
