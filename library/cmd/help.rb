2f76323bc93e217175a54e824cafc0027c44393a
Alexander Zhukov <zuav@crystax.net>
2016-01-16 16:58:14 +0300

    Add 'help command' support
    Add description of the build command options

--- library/cmd/help.rb
+++ library/cmd/help.rb
@@ -1,4 +1,5 @@
 require_relative '../exceptions.rb'
+require_relative '../build.rb'
 
 
 CREW_HELP = <<-EOS
@@ -12,7 +13,9 @@ OPTIONS are:
 
 COMMAND is one of the following:
   version         output version information
-  help            show this help message
+  help [command]...
+                  if no command specified show this help message;
+                  otherwise show addition info for the specified commands
   env             show crew's command working environment
   list [libs|utils]
                   list all available formulas for libraries or utilities;
@@ -24,7 +27,7 @@ COMMAND is one of the following:
                   uninstall the specified formulas
   source name[[:version]:crystax_version] ...
                   install source code for the specified formula(s)
-  build name[[:version]:crystax_version] ...
+  build [build_options] name[:version] ...
                   build the specified formula(s) from the source code
   remove-source name[:version] ...
                   remove source code for the specified formulas
@@ -34,11 +37,59 @@ COMMAND is one of the following:
 EOS
 
 
+NO_HELP = <<-EOS
+There is no additional info the command.
+EOS
+
+BUILD_HELP = <<-EOS
+The command support the following options:
+
+--abis=LIST      the list of ABIs, for which to build formula;
+                 ABIs must be separted with comma;
+                 available ABIs are armeabi, armeabi-v7a, armeabi-v7a-hard,
+                 x86, mips, arm64-v8a, x86_64, mips64
+                 by default the formula will be built for all ABIs
+
+--build-only     do not create package in the cache dir and do not install
+                 formula
+
+--no-clean       do not remove build artifacts
+
+--update-shasum  calculate SHA256 sum of the package and update formula
+
+--num-jobs=N     set number of jobs for a make commad;
+                 default value depends on the machine used
+
+EOS
+
+CMD_HELP = {
+  'version'       => NO_HELP,
+  'help'          => NO_HELP,
+  'env'           => NO_HELP,
+  'list'          => NO_HELP,
+  'info'          => NO_HELP,
+  'install'       => NO_HELP,
+  'remove'        => NO_HELP,
+  'source'        => NO_HELP,
+  'build'         => BUILD_HELP,
+  'remove-source' => NO_HELP,
+  'update'        => NO_HELP,
+  'upgrade'       => NO_HELP,
+  'cleanup'       => NO_HELP
+}
+
 module Crew
   def self.help(args)
-    if args and args.length > 0
-      raise CommandRequresNoArguments
+    case args.size
+    when 0
+      puts CREW_HELP
+    else
+      args.each do |cmd|
+        raise "unknown command '#{cmd}'" unless CMD_HELP.keys.include? cmd
+        puts "'#{cmd.upcase}'"
+        puts ''
+        puts CMD_HELP[cmd]
+      end
     end
-    puts CREW_HELP
   end
 end
