d36f34be36d4a4f05f304e894a92d921231de679
Alexander Zhukov <zuav@crystax.net>
2016-01-26 17:42:14 +0300

    Add simple unit tests for Formula class

---
+++ test/data/simple.rb
@@ -0,0 +1,7 @@
+class Simple < Formula
+
+  desc "Simple Formula"
+  homepage "http://www.simple.formula.org"
+
+  release version: '1.0.0', crystax_version: 1, sha256: '0'
+end
--- test/test_formula.rb
+++ test/test_formula.rb
@@ -1,8 +1,9 @@
 require 'minitest/unit'
+require_relative 'test_consts.rb'
 require_relative '../library/formulary.rb'
 require_relative '../library/formula.rb'
 
-class TestRelease < MiniTest::Test
+class TestFormula < MiniTest::Test
 
   def test_package_version
     r = Release.new('1.2.3', 4)
@@ -16,8 +17,24 @@ class TestRelease < MiniTest::Test
     assert_equal(r.crystax_version, cxver)
   end
 
-  # def test_simple_formula
-  #   f = Formulary.factory('data/simple.rb')
-  #   assert_equal(f.name, 'Simple')
-  # end
+  def test_simple_formula
+    r1 = Release.new('1.0.0', 1, '0')
+    f = Formulary.factory(File.join(Dir.pwd, Crew_test::DATA_DIR, 'simple.rb'))
+    assert_equal('simple',                        f.name)
+    assert_equal('Simple Formula',                f.desc)
+    assert_equal('http://www.simple.formula.org', f.homepage)
+    assert_equal(1,                               f.releases.size)
+    assert_equal('1.0.0',                         f.releases[0].version)
+    assert_equal(1,                               f.releases[0].crystax_version)
+    assert_equal('0',                             f.releases[0].shasum)
+    assert_equal([],                              f.dependencies)
+    # todo:
+    #assert_equal([],                              f.full_dependencies)
+    # cache_file
+    # install
+    assert_equal(false,                           f.installed?)
+    assert_equal(false,                           f.installed?(r1))
+    assert_equal(false,                           f.source_installed?)
+    assert_equal(false,                           f.source_installed?(r1))
+  end
 end
