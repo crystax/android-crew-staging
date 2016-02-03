7d8a5b7912b7c3215c929f93fd5e073bbb59f2a1
Alexander Zhukov <zuav@crystax.net>
2016-02-03 22:38:21 +0300

    Build libssh2 for darwin

---
+++ formula/utilities/libssh2.rb
@@ -0,0 +1,44 @@
+class Libssh2 < Utility
+
+  desc "A Massively Spiffy Yet Delicately Unobtrusive Compression Library"
+  homepage 'http://www.libssh2.org/'
+  url 'http://www.libssh2.org/download/libssh2-${version}.tar.gz'
+
+  release version: '1.6.0', crystax_version: 1, sha256: { linux_x86_64:   '0',
+                                                          linux_x86:      '0',
+                                                          darwin_x86_64:  '0',
+                                                          darwin_x86:     '0',
+                                                          windows_x86_64: '0',
+                                                          windows:        '0'
+                                                        }
+
+  build_depends_on 'zlib'
+  build_depends_on 'openssl'
+
+  def build_for_platform(platform, release, options, dep_dirs)
+    install_dir = install_dir_for_platform(platform, release)
+    zlib_dir    = dep_dirs[platform.name]['zlib']
+    openssl_dir = dep_dirs[platform.name]['openssl']
+
+    build_env['CC']      = platform.cc
+    build_env['CFLAGS']  = "-I#{openssl_dir}/include -I#{zlib_dir}/include #{platform.cflags}"
+    build_env['LDFLAGS'] = "-L#{openssl_dir}/lib -L#{zlib_dir}/lib -lz"
+    build_env['LIBS']    = "-lgdi32" if platform.target_os == 'windows'
+    build_env['LIBS']    = "-ldl"    if platform.target_os == 'linux'
+    #build_env['DESTDIR'] = install_dir
+    #build_env['PATH']    = '/bin:/usr/bin:/sbin:/usr/sbin'
+
+    args = ["--prefix=#{install_dir}",
+            "--host=#{platform.configure_host}",
+            "--disable-shared",
+            "--disable-examples-build",
+            "--with-libssl-prefix=#{openssl_dir}",
+            "--with-libz=#{zlib_dir}"
+           ]
+
+    system "#{src_dir}/configure",  *args
+    system 'make', '-j', num_jobs
+    system 'make', 'check' if options.check? platform
+    system 'make', 'install'
+  end
+end
