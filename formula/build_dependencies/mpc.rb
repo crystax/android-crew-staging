03b686c299ef83a214483cb0fdc437a3e59fd5b3
Alexander Zhukov <zuav@crystax.net>
2016-07-20 18:07:31 +0300

    Build mpfr and mpc for darwin

---
+++ formula/build_dependencies/mpc.rb
@@ -0,0 +1,39 @@
+class Mpc < BuildDependency
+
+  desc "C library for the arithmetic of high precision complex numbers"
+  homepage "http://multiprecision.org"
+  url "https://ftpmirror.gnu.org/mpc/mpc-${version}.tar.gz"
+
+  release version: '1.0.3', crystax_version: 1, sha256: { linux_x86_64:   '0',
+                                                          darwin_x86_64:  '0',
+                                                          windows_x86_64: '0',
+                                                          windows:        '0'
+                                                        }
+
+  depends_on 'gmp'
+  depends_on 'mpfr'
+
+  def build_for_platform(platform, release, options, dep_dirs)
+    install_dir = install_dir_for_platform(platform, release)
+
+    gmp_dir = dep_dirs[platform.name]['gmp']
+    mpfr_dir = dep_dirs[platform.name]['mpfr']
+
+    args = ["--prefix=#{install_dir}",
+            "--host=#{platform.configure_host}",
+            "--with-gmp=#{gmp_dir}",
+            "--with-mpfr=#{mpfr_dir}",
+            "--disable-shared",
+            "--disable-silent-rules",
+            "--with-sysroot"
+           ]
+
+    system "#{src_dir}/configure", *args
+    system 'make', '-j', num_jobs
+    system 'make', 'check' if options.check? platform
+    system 'make', 'install'
+
+    # remove unneeded files before packaging
+    FileUtils.cd(install_dir) { FileUtils.rm_rf ['share'] + Dir['lib/*.la'] }
+  end
+end
---
+++ formula/build_dependencies/mpfr.rb
@@ -0,0 +1,36 @@
+class Mpfr < BuildDependency
+
+  desc "C library for multiple-precision floating-point computations"
+  homepage "http://www.mpfr.org/"
+  url "https://mirrors.ocf.berkeley.edu/debian/pool/main/m/mpfr4/mpfr4_${version}.orig.tar.xz"
+
+  release version: '3.1.4', crystax_version: 1, sha256: { linux_x86_64:   '0',
+                                                          darwin_x86_64:  '0',
+                                                          windows_x86_64: '0',
+                                                          windows:        '0'
+                                                        }
+
+  depends_on 'gmp'
+
+  def build_for_platform(platform, release, options, dep_dirs)
+    install_dir = install_dir_for_platform(platform, release)
+
+    gmp_dir = dep_dirs[platform.name]['gmp']
+
+    args = ["--prefix=#{install_dir}",
+            "--host=#{platform.configure_host}",
+            "--with-gmp=#{gmp_dir}",
+            "--disable-shared",
+            "--disable-silent-rules",
+            "--with-sysroot"
+           ]
+
+    system "#{src_dir}/configure", *args
+    system 'make', '-j', num_jobs
+    system 'make', 'check' if options.check? platform
+    system 'make', 'install'
+
+    # remove unneeded files before packaging
+    FileUtils.cd(install_dir) { FileUtils.rm_rf ['share'] + Dir['lib/*.la'] }
+  end
+end
