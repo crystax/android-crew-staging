938cab0e19d2e7cc0487a0ae68f382a442ce57da
Alexander Zhukov <zuav@crystax.net>
2016-08-10 14:17:50 +0300

    Add toochain_build and toolchain_host attributes

--- library/platform.rb
+++ library/platform.rb
@@ -27,7 +27,7 @@ class Platform
                              }
               }
 
-  attr_reader :name, :target_os, :target_cpu, :cc, :cxx, :ar, :ranlib, :cflags, :cxxflags, :configure_host #:toolchain_host, :toolchain_build
+  attr_reader :name, :target_os, :target_cpu, :cc, :cxx, :ar, :ranlib, :cflags, :cxxflags, :configure_host, :toolchain_host, :toolchain_build
 
   def initialize(name)
     raise "unsupported platform #{name}" unless NAMES.include? name
@@ -54,23 +54,23 @@ class Platform
     when 'darwin-x86_64'
       @cflags          = "-isysroot#{Global::PLATFORM_PREBUILTS_DIR}/sysroot/darwin-x86/MacOSX10.6.sdk -mmacosx-version-min=#{MACOSX_VERSION_MIN} -DMAXOSX_DEPLOYEMENT_TARGET=#{MACOSX_VERSION_MIN} -m64"
       @configure_host  = 'x86_64-darwin10'
-      # @toolchain_host  = 'x86_64-apple-darwin'
-      # @toolchain_build = @toolchain_host
+      @toolchain_build = 'x86_64-apple-darwin'
+      @toolchain_host  = @toolchain_build
     when 'linux-x86_64'
       @cflags          = "--sysroot=#{Global::PLATFORM_PREBUILTS_DIR}/gcc/linux-x86/host/x86_64-linux-glibc2.11-4.8/sysroot"
       @configure_host  = 'x86_64-linux'
-      # @toolchain_host  = 'x86_64-linux-gnu'
-      # @toolchain_build = @toolchain_host
+      @toolchain_build = 'x86_64-linux-gnu'
+      @toolchain_host  = @toolchain_build
     when 'windows-x86_64'
       @cflags          = '-m64'
       @configure_host  = 'x86_64-w64-mingw32'
-      # @toolchain_host  = 'x86_64-pc-mingw32msvc'
-      # @toolchain_build = 'i686-pc-cygwin'
+      @toolchain_build = 'i686-pc-cygwin'
+      @toolchain_host  = 'x86_64-pc-mingw32msvc'
     when 'windows'
       @cflags          = '-m32'
       @configure_host  = 'x86_64-w64-mingw32'
-      # @toolchain_host  = 'i586-pc-mingw32msvc'
-      # @toolchain_build = 'i686-pc-cygwin'
+      @toolchain_build = 'i686-pc-cygwin'
+      @toolchain_host  = 'i586-pc-mingw32msvc'
     end
 
     @cxxflags = @cflags
