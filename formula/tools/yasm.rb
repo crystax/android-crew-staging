b6f5bdb13b1715feb2a5b11defe63e914c3d84b5
Alexander Zhukov <zuav@crystax.net>
2016-08-17 17:54:35 +0300

    Add formula for yasm

---
+++ formula/tools/yasm.rb
@@ -0,0 +1,30 @@
+class Yasm < Utility
+
+  desc "Modular BSD reimplementation of NASM"
+  homepage "http://yasm.tortall.net/"
+  url "https://www.tortall.net/projects/yasm/releases/yasm-${version}.tar.gz"
+
+  release version: '1.3.0', crystax_version: 1, sha256: { linux_x86_64:   '0',
+                                                          darwin_x86_64:  '0',
+                                                          windows_x86_64: '0',
+                                                          windows:        '0'
+                                                        }
+
+  def build_for_platform(platform, release, options, _host_dep_dirs, _target_dep_dirs)
+    install_dir = install_dir_for_platform(platform, release)
+
+    args = ["--prefix=#{install_dir}",
+            "--host=#{platform.configure_host}",
+            "--disable-nls",
+            "--disable-rpath"
+           ]
+
+    system "#{src_dir}/configure", *args
+    system 'make', '-j', num_jobs
+    system 'make', 'test' if options.check? platform
+    system 'make', 'install'
+
+    # remove unneeded files before packaging
+    FileUtils.cd(install_dir) { FileUtils.rm_rf(['include', 'lib', 'share']) }
+  end
+end
