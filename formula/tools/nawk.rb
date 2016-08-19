1dc8dd8fa228ef7140d7a6cf2942593bca561efe
Alexander Zhukov <zuav@crystax.net>
2016-08-19 13:46:09 +0300

    Build nawk for linux and windows platforms

--- formula/tools/nawk.rb
+++ formula/tools/nawk.rb
@@ -15,9 +15,9 @@ class Nawk < Utility
   def build_for_platform(platform, release, options, _host_dep_dirs, _target_dep_dirs)
     build_env['BUILD_DIR']     = Dir.pwd
     build_env['BUILD_MINGW']   = 'yes' if platform.target_os == 'windows'
-    build_env['BUILD_TRY64']   = 'yes' if platform.target_cpu == 'x86_64'
     build_env['HOST_CC']       = platform.cc
     build_env['CFLAGS']       += ' -O2 -s'
+    build_env['LDFLAGS']       = ' -m32' if platform.name == 'windows'
     build_env['NATIVE_CC']     = 'gcc'
     build_env['NATIVE_CFLAGS'] = " -O2 -s -I#{Dir.pwd} -I."
     build_env['V']             = '1'
