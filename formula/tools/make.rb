458f5ea399f262d3de0ac4e5d61d6840a4d14999
Alexander Zhukov <zuav@crystax.net>
2016-09-05 13:51:35 +0300

    Use sources from sources/host-tools for awk and make

--- formula/tools/make.rb
+++ formula/tools/make.rb
@@ -2,7 +2,7 @@ class Make < Utility
 
   desc "Utility for directing compilation"
   homepage "https://www.gnu.org/software/make/"
-  url "https://ftpmirror.gnu.org/make/make-${version}.tar.bz2"
+  #url "https://ftpmirror.gnu.org/make/make-${version}.tar.bz2"
 
   release version: '3.81', crystax_version: 1, sha256: { linux_x86_64:   '0',
                                                          darwin_x86_64:  '0',
@@ -10,7 +10,12 @@ class Make < Utility
                                                          windows:        '0'
                                                        }
 
+  def prepare_source_code(release, dir, src_name, log_prefix)
+    # source code is in soruce/host-tools/ directory
+  end
+
   def build_for_platform(platform, release, options, _host_dep_dirs, _target_dep_dirs)
+    src_dir = File.join(Build::NDK_HOST_TOOLS_DIR, "make-#{release.version}")
     install_dir = install_dir_for_platform(platform, release)
 
     args = ["--prefix=#{install_dir}",
@@ -25,6 +30,6 @@ class Make < Utility
     system 'make', 'install'
 
     # remove unneeded files before packaging
-    FileUtils.cd(install_dir) { FileUtils.rm_rf(['info', 'man']) }
+    FileUtils.cd(install_dir) { FileUtils.rm_rf 'share' }
   end
 end
--- formula/tools/nawk.rb
+++ formula/tools/nawk.rb
@@ -2,17 +2,23 @@ class Nawk < Utility
 
   desc "The One True Awk"
   homepage "https://www.cs.princeton.edu/~bwk/btl.mirror/"
-  url "https://www.cs.princeton.edu/~bwk/btl.mirror/awk.tar.gz"
+  #url "https://www.cs.princeton.edu/~bwk/btl.mirror/awk.tar.gz"
 
-  release version: '20120120', crystax_version: 1, sha256: { linux_x86_64:   '0',
+  release version: '20071023', crystax_version: 1, sha256: { linux_x86_64:   '0',
                                                              darwin_x86_64:  '0',
                                                              windows_x86_64: '0',
                                                              windows:        '0'
                                                            }
 
-  build_options source_archive_without_top_dir: true
+  #build_options source_archive_without_top_dir: true
+
+  def prepare_source_code(release, dir, src_name, log_prefix)
+    # source code is in soruce/host-tools/ directory
+  end
 
   def build_for_platform(platform, release, options, _host_dep_dirs, _target_dep_dirs)
+    src_dir = File.join(Build::NDK_HOST_TOOLS_DIR, "nawk-#{release.version}")
+
     build_env['BUILD_DIR']     = Dir.pwd
     build_env['BUILD_MINGW']   = 'yes' if platform.target_os == 'windows'
     build_env['HOST_CC']       = platform.cc
@@ -26,6 +32,6 @@ class Nawk < Utility
 
     bin_dir = File.join(install_dir_for_platform(platform, release), 'bin')
     FileUtils.mkdir_p bin_dir
-    FileUtils.cp "awk#{platform.target_exe_ext}", bin_dir
+    FileUtils.cp "ndk-awk#{platform.target_exe_ext}", "#{bin_dir}/awk#{platform.target_exe_ext}"
   end
 end
