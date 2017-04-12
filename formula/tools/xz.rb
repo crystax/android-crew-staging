ef531c373c1dea74853c222fca692a192462ebe4
Alexander Zhukov <zuav@crystax.net>
2017-04-12 17:29:17 +0300

    [xz] Update to version 5.2.3

--- formula/tools/xz.rb
+++ formula/tools/xz.rb
@@ -4,7 +4,7 @@ class Xz < BuildDependency
   homepage "http://tukaani.org/xz/"
   url "http://tukaani.org/xz/xz-${version}.tar.xz"
 
-  release version: '5.2.2', crystax_version: 1, sha256: { linux_x86_64:   '3ca8d84b5ae91aa073e335e15bbb14cfa453e2e098abdf595e19738a3e91b042',
+  release version: '5.2.3', crystax_version: 1, sha256: { linux_x86_64:   '3ca8d84b5ae91aa073e335e15bbb14cfa453e2e098abdf595e19738a3e91b042',
                                                           darwin_x86_64:  'c1e49d93603c672ae786853de896583a999f65264ea4bb8deecf288b7ad8b9bb',
                                                           windows_x86_64: 'f8041496bfd4c299c2962a0adcca64614f3a2b21fe1cbae926f9d184112ae3ac',
                                                           windows:        'e11adeeae2849183f876fabb71282ce4f16ab3276283efdd213f8d060bd74866'
