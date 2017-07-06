9c15aa094c4b0ce25536052fa68157692b18deed
Alexander Zhukov <zuav@crystax.net>
2017-07-06 14:46:05 +0300

    [tests] Fix spec_cleanup procedure

--- test/spec_cleanup.rb
+++ test/spec_cleanup.rb
@@ -3,10 +3,10 @@ require_relative 'spec_consts.rb'
 
 
 FileUtils.rm_rf 'tmp'
+FileUtils.rm_rf Crew::Test::NDK_DIR
 FileUtils.rm_rf Crew::Test::NDK_COPY_DIR
 FileUtils.rm_rf Crew::Test::PKG_CACHE_BASE
-FileUtils.rm_rf File.join(Crew::Test::NDK_DIR, 'prebuilt')
-FileUtils.rm_rf File.join(Crew::Test::DOCROOT_DIR, 'utilities')
+FileUtils.rm_rf File.join(Crew::Test::DOCROOT_DIR, 'tools')
 
 FileUtils.cd(Crew::Test::DATA_DIR) { FileUtils.rm Dir['curl-*.rb', 'libarchive-*.rb', 'ruby-*.rb', 'xz-*.rb', 'releases_info.rb'] }
 FileUtils.rm_f Crew::Test::DATA_READY_FILE
