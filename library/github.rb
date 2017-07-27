18e2e4b863faa50e83be19deacfb9e355a50b5a7
Alexander Zhukov <zuav@crystax.net>
2017-07-27 17:19:11 +0300

    Add more constants to github module

--- library/github.rb
+++ library/github.rb
@@ -1,7 +1,12 @@
 module GitHub
 
-  STAGING_SSH_URL       = 'git@github.com:crystax/android-crew-staging.git'
-  STAGING_HTTPS_URL     = 'https://github.com/crystax/android-crew-staging.git'
-  STAGING_DONWLOAD_BASE = 'https://api.github.com/repos/crystax/android-crew-staging/releases/7122171/assets'
+  OWNER = 'crystax'
+
+  STAGING_REPO_NAME  = 'android-crew-staging'
+  STAGING_RELEASE_ID = '7122171'
+
+  STAGING_SSH_URL       = "git@github.com:#{OWNER}/#{STAGING_REPO_NAME}.git"
+  STAGING_HTTPS_URL     = "https://github.com/#{OWNER}/#{STAGING_REPO_NAME}.git"
+  STAGING_DONWLOAD_BASE = "https://api.github.com/repos/#{OWNER}/#{STAGING_REPO_NAME}/releases/#{STAGING_RELEASE_ID}/assets"
 
 end
