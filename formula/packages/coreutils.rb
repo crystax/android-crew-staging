d3bad0dfd6187c030b60b6daa12967da9124ff12
Alexander Zhukov <zuav@crystax.net>
2017-08-03 17:08:26 +0300

    [coreutils] Update to version 8.27
    Also try to fix build on darwin systems.

--- formula/packages/coreutils.rb
+++ formula/packages/coreutils.rb
@@ -2,9 +2,9 @@ class Coreutils < Package
 
   desc "GNU File, Shell, and Text utilities"
   homepage "https://www.gnu.org/software/coreutils"
-  url "http://ftpmirror.gnu.org/coreutils/coreutils-8.25.tar.xz"
+  url "http://ftpmirror.gnu.org/coreutils/coreutils-${version}.tar.xz"
 
-  release version: '8.25', crystax_version: 1
+  release version: '8.27', crystax_version: 1
 
   build_options copy_installed_dirs: ['bin'],
                 gen_android_mk:      false
@@ -24,6 +24,8 @@ class Coreutils < Package
 	      "--without-libpth-prefix",
 	      "--without-libintl-prefix"
             ]
+
+    FileUtils.touch 'configure'
 
     system './configure', *args
     system 'make', '-j', num_jobs, 'V=1'
