893e967b54d982eda5e6307246a3a97b746c55c8
Alexander Zhukov <zuav@crystax.net>
2017-09-06 22:11:40 +0300

    [libssh2] Build as utility with dev files
    Build as shared libs.
    Fix linker error when linking with shared openssl 1.1.* with static engines.

--- formula/tools/libssh2.rb
+++ formula/tools/libssh2.rb
@@ -1,4 +1,4 @@
-class Libssh2 < BuildDependency
+class Libssh2 < Utility
 
   desc "A Massively Spiffy Yet Delicately Unobtrusive Compression Library"
   homepage 'http://www.libssh2.org/'
@@ -7,24 +7,34 @@ class Libssh2 < BuildDependency
   release version: '1.8.0', crystax_version: 2
 
   depends_on 'zlib'
-  # todo: depends on openssl 1.0.*
   depends_on 'openssl'
 
   def build_for_platform(platform, release, options, host_dep_dirs, _target_dep_dirs)
     install_dir = install_dir_for_platform(platform.name, release)
-    zlib_dir    = host_dep_dirs[platform.name]['zlib']
-    openssl_dir = host_dep_dirs[platform.name]['openssl']
+    tools_dir   = Global::tools_dir(platform.name)
 
-    build_env['CFLAGS']  += " -I#{openssl_dir}/include -I#{zlib_dir}/include #{platform.cflags}"
-    build_env['LDFLAGS']  = "-L#{openssl_dir}/lib -L#{zlib_dir}/lib -lz"
+    if platform.target_os == 'windows'
+      crypto = 'crypto.dll'
+      ssl    = 'ssl.dll'
+      zlib   = 'z.dll'
+    else
+      crypto = 'crypto'
+      ssl    = 'ssl'
+      zlib = 'z'
+    end
+
+    build_env['CFLAGS']  += " -I#{tools_dir}/include #{platform.cflags}"
+    build_env['LDFLAGS']  = "-L#{tools_dir}/lib -l#{ssl} -l#{crypto} -l#{zlib}"
     build_env['LIBS']     = "-lcrypt32 -lgdi32" if platform.target_os == 'windows'
     build_env['LIBS']     = "-ldl"              if platform.target_os == 'linux'
+
+    build_env['LD_LIBRARY_PATH'] = "#{tools_dir}/lib" unless platform.target_os == 'windows'
 
     args = platform.configure_args +
            ["--prefix=#{install_dir}",
             "--disable-examples-build",
-            "--with-libssl-prefix=#{openssl_dir}",
-            "--with-libz=#{zlib_dir}"
+            "--with-libssl-prefix=#{tools_dir}",
+            "--with-libz=#{tools_dir}"
            ]
 
     system "#{src_dir}/configure",  *args
@@ -36,5 +46,9 @@ class Libssh2 < BuildDependency
     FileUtils.rm_rf "#{install_dir}/lib/pkgconfig"
     FileUtils.rm_rf "#{install_dir}/share"
     FileUtils.rm_rf Dir["#{install_dir}/lib/*.la"]
+  end
+
+  def split_file_list(list, platform_name)
+    split_file_list_by_shared_libs(list, platform_name)
   end
 end
