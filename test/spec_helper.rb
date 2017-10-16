615bd1cb6d64e2bff6eeea910039edb138669774
Alexander Zhukov <zuav@crystax.net>
2017-10-16 14:17:52 +0300

    Use File.dirname instead of Dir.pwd

--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -26,16 +26,20 @@ FileUtils.mkdir_p File.join(Crew::Test::CREW_DIR, 'formula')
 FileUtils.mkdir_p File.join(Crew::Test::CREW_DIR, 'patches')
 
 ENV['CREW_DOWNLOAD_BASE'] = Crew::Test::DOWNLOAD_BASE
-ENV['CREW_BASE_DIR']      = "#{Dir.pwd}/#{Crew::Test::CREW_DIR}"
-ENV['CREW_NDK_DIR']       = "#{Dir.pwd}/#{Crew::Test::NDK_DIR}"
-ENV['CREW_PKG_CACHE_DIR'] = "#{Dir.pwd}/#{Crew::Test::PKG_CACHE_DIR}"
-ENV['CREW_SRC_CACHE_DIR'] = "#{Dir.pwd}/#{Crew::Test::SRC_CACHE_DIR}"
+ENV['CREW_BASE_DIR']      = "#{File.dirname(__FILE__)}/#{Crew::Test::CREW_DIR}"
+ENV['CREW_NDK_DIR']       = "#{File.dirname(__FILE__)}/#{Crew::Test::NDK_DIR}"
+ENV['CREW_PKG_CACHE_DIR'] = "#{File.dirname(__FILE__)}/#{Crew::Test::PKG_CACHE_DIR}"
+ENV['CREW_SRC_CACHE_DIR'] = "#{File.dirname(__FILE__)}/#{Crew::Test::SRC_CACHE_DIR}"
 
 # global.rb requires evn vars to be set so we put it here
 require_relative '../library/global.rb'
+
 # helpers.rb uses constants from 'global.rb'
 require_relative 'helpers.rb'
 
+
+require 'rspec/expectations'
+RSpec::Support::ObjectFormatter.default_instance.max_formatted_output_length = 200000
 
 RSpec.configure do |config|
   config.include Spec::Helpers
