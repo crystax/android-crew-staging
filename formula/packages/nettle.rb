b203370b916510203296dd6354525080dc251e11
Alexander Zhukov <zuav@crystax.net>
2018-02-03 14:21:52 +0300

    [target/nettle] Build for all abis

---
+++ formula/packages/nettle.rb
@@ -0,0 +1,53 @@
+class Nettle < Package
+
+  desc "A low-level cryptographic library"
+  homepage "https://www.lysator.liu.se/~nisse/nettle/"
+  url "https://ftp.gnu.org/gnu/nettle/nettle-${version}.tar.gz"
+
+  release version: '3.4', crystax_version: 1
+
+  depends_on 'openssl'
+  depends_on 'gmp'
+
+  build_copy 'COPYING.LESSERv3', 'COPYINGv2', 'COPYINGv3'
+
+  build_options use_cxx: true
+
+  #               ldflags_in_c_wrapper: true
+
+  def build_for_abi(abi, _toolchain, _release, _host_dep_dirs, target_dep_dirs, _options)
+    install_dir = install_dir_for_abi(abi)
+    openssl_dir = target_dep_dirs['openssl']
+    gmp_dir     = target_dep_dirs['gmp']
+
+    args =  [ "--prefix=#{install_dir}",
+              "--host=#{host_for_abi(abi)}",
+              "--enable-shared",
+              "--enable-static",
+              "--disable-openssl",
+              "--disable-documentation",
+              "--with-include-path=#{openssl_dir}/include:#{gmp_dir}/include",
+              "--with-lib-path=#{openssl_dir}/libs/#{abi}:#{gmp_dir}/libs/#{abi}"
+            ]
+
+    #args << "--disable-assembly" if abi == 'mips64'
+
+    system './configure', *args
+    system 'make', '-j', num_jobs
+    system 'make', 'install'
+
+    # cleanup lib directory
+    FileUtils.cd("#{install_dir}/lib") do
+      FileUtils.rm_rf 'pkgconfig'
+      FileUtils.rm ['libhogweed.so', 'libnettle.so', 'libhogweed.so.4', 'libnettle.so.6']
+      FileUtils.mv 'libhogweed.so.4.4', 'libhogweed.so'
+      FileUtils.mv 'libnettle.so.6.4',  'libnettle.so'
+    end
+  end
+
+  def sonames_translation_table(_release)
+    { 'libhogweed.so.4' => 'libhogweed',
+      'libnettle.so.6'  => 'libnettle'
+    }
+  end
+end
