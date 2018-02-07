a0ab0d15333e9fe28f1ef33e98282be3d87edd4b
Alexander Zhukov <zuav@crystax.net>
2018-02-07 22:21:33 +0300

    [cpulimit] Build for all abis

---
+++ formula/packages/cpulimit.rb
@@ -0,0 +1,22 @@
+class Cpulimit < Package
+
+  desc "Cpulimit is a tool which limits the CPU usage of a process"
+  homepage "https://github.com/opsengine/cpulimit"
+  url "https://github.com/opsengine/cpulimit/archive/v${version}.tar.gz"
+
+  release version: '0.2', crystax_version: 1
+
+  build_copy 'LICENSE'
+  build_options ldflags_in_c_wrapper: true,
+                copy_installed_dirs:  ['bin'],
+                gen_android_mk:       false
+
+
+  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, _target_dep_dirs, _options)
+    system 'make', '-j', num_jobs
+
+    bin_dir = "#{install_dir_for_abi(abi)}/bin"
+    FileUtils.mkdir_p bin_dir
+    FileUtils.cp 'src/cpulimit', bin_dir
+  end
+end
