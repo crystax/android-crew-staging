347e97c5fbdcb236a94c195b0d31e22c2b922f58
Alexander Zhukov <zuav@crystax.net>
2018-02-08 17:04:17 +0300

    [gnu-which] Build for all abis

---
+++ formula/packages/gnu_which.rb
@@ -0,0 +1,27 @@
+class GnuWhich < Package
+
+  name 'gnu-which'
+  desc 'GNU which shows the full path of (shell) commands'
+  homepage 'http://carlowood.github.io/which/index.html'
+  url 'http://carlowood.github.io/which/which-${version}.tar.gz'
+
+  release version: '2.21', crystax_version: 1
+
+  build_copy 'COPYING'
+  build_options copy_installed_dirs: ['bin'],
+                gen_android_mk:      false
+
+  def build_for_abi(abi, _toolchain, _release, _host_dep_dirs, target_dep_dirs, _options)
+    install_dir = install_dir_for_abi(abi)
+    ncurses_dir = target_dep_dirs['ncurses']
+
+    args =  ["--prefix=#{install_dir}",
+             "--host=#{host_for_abi(abi)}",
+             "--disable-silent-rules"
+            ]
+
+    system './configure', *args
+    system 'make', '-j', num_jobs
+    system 'make', 'install'
+  end
+end
