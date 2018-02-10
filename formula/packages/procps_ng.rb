d7e84d7cfe313073c3e895c2be989fd6727872d5
Alexander Zhukov <zuav@crystax.net>
2018-02-10 16:06:33 +0300

    [procps-ng] Build for all abis on linux

--- formula/packages/procps_ng.rb
+++ formula/packages/procps_ng.rb
@@ -10,15 +10,17 @@ class ProcpsNg < Package
   depends_on 'ncurses'
 
   build_copy 'COPYING'
-  build_options copy_installed_dirs: ['bin'],
-                gen_android_mk:      false
+  build_options copy_installed_dirs:  ['bin', 'include', 'lib', 'sbin'],
+                gen_android_mk:       false
 
   def build_for_abi(abi, _toolchain, _release, _host_dep_dirs, target_dep_dirs, _options)
     install_dir = install_dir_for_abi(abi)
     ncurses_dir = target_dep_dirs['ncurses']
 
-    build_env['CFLAGS']  += " -I#{ncurses_dir}/include -L#{ncurses_dir}/libs/#{abi}"
-    build_env['LIBS']     = '-ltinfow'
+    # todo: remove HOST_NAME_MAX when libcrystax is fixed
+    build_env['CFLAGS'] += " -I#{ncurses_dir}/include -I#{ncurses_dir}/include/ncursesw -L#{ncurses_dir}/libs/#{abi} -DHOST_NAME_MAX=255"
+    build_env['LIBS']    = '-ltinfow'
+    build_env['PATH']    = "/usr/local/opt/gettext/bin:#{ENV['PATH']}" if Global::OS == 'darwin'
 
     args =  [ "--prefix=#{install_dir}",
               "--host=#{host_for_abi(abi)}",
@@ -31,8 +33,23 @@ class ProcpsNg < Package
 	      "--disable-rpath"
             ]
 
+    system './autogen.sh'
+    set_ac_cv
     system './configure', *args
+    unset_ac_cv
     system 'make', '-j', num_jobs
     system 'make', 'install'
+
+    clean_install_dir abi, :lib
+  end
+
+  def set_ac_cv
+    build_env['ac_cv_func_malloc_0_nonnull'] = 'yes'
+    build_env['ac_cv_func_realloc_0_nonnull'] = 'yes'
+  end
+
+  def unset_ac_cv
+    build_env.delete('ac_cv_func_malloc_0_nonnull')
+    build_env.delete('ac_cv_func_realloc_0_nonnull')
   end
 end
