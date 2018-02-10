080768fe240ddc9fbeae94326a9f0592e1576bcd
Alexander Zhukov <zuav@crystax.net>
2018-02-10 21:37:30 +0300

    [info-zip] Build for all abis on darwin

---
+++ formula/packages/info_zip.rb
@@ -0,0 +1,26 @@
+class InfoZip < Package
+
+  name 'info-zip'
+  desc 'Zip - a compressor/archiver for creating and modifying zipfiles'
+  homepage 'http://www.info-zip.org/Zip.html'
+  url 'https://sourceforge.net/projects/infozip/files/Zip%203.x%20%28latest%29/${version}/zip${block}.tar.gz'  do |r| r.version.split('.').first(2).join end
+
+  release version: '3.0', crystax_version: 1
+
+  build_copy 'LICENSE'
+  build_options cflags_in_c_wrapper:  true,
+                ldflags_in_c_wrapper: true,
+                copy_installed_dirs:  ['bin'],
+                gen_android_mk:       false
+
+
+  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, _target_dep_dirs, _options)
+    build_env['PATH'] = "#{Dir.pwd}:#{ENV['PATH']}"
+
+    system "make -j #{num_jobs} -f unix/Makefile generic"
+
+    install_bin_dir = "#{install_dir_for_abi(abi)}/bin"
+    FileUtils.mkdir_p install_bin_dir
+    FileUtils.cp 'zip', install_bin_dir
+  end
+end
