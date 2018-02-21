c9e36fe4c5357c0cea02059f3d308c731026732b
Alexander Zhukov <zuav@crystax.net>
2018-02-21 20:02:10 +0700

    [target/xz] Build for all abis on darwin

---
+++ formula/packages/xz.rb
@@ -0,0 +1,35 @@
+class Xz < Package
+
+  desc "General-purpose data compression with high compression ratio"
+  homepage "http://tukaani.org/xz/"
+  url "http://tukaani.org/xz/xz-${version}.tar.xz"
+
+  release version: '5.2.3', crystax_version: 3
+
+  build_copy 'COPYING'
+  build_options build_outside_source_tree: true,
+                copy_installed_dirs: ['bin', 'include', 'lib']
+
+  def build_for_abi(abi, _toolchain,  release, _host_dep_dirs, _target_dep_dirs, _options)
+    install_dir = install_dir_for_abi(abi)
+    src_dir = source_directory(release)
+
+    args =  [ "--prefix=#{install_dir}",
+              "--host=#{host_for_abi(abi)}",
+              "--disable-silent-rules",
+              "--disable-nls",
+              "--disable-doc",
+              "--disable-lzma-links",
+              "--enable-shared",
+              "--enable-static",
+              "--with-pic",
+              "--with-sysroot"
+            ]
+
+    system "#{src_dir}/configure", *args
+    system 'make', '-j', num_jobs
+    system 'make', 'install'
+
+    clean_install_dir abi, :lib
+  end
+end
