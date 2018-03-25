0a11e388470d0155e1b4ec0a93d0c743606a60ec
Alexander Zhukov <zuav@crystax.net>
2018-03-25 13:33:18 +0300

    [coreutils] Add script to recreate symlinks

--- formula/packages/coreutils.rb
+++ formula/packages/coreutils.rb
@@ -4,11 +4,14 @@ class Coreutils < Package
   homepage "https://www.gnu.org/software/coreutils"
   url "http://ftpmirror.gnu.org/coreutils/coreutils-${version}.tar.xz"
 
-  release version: '8.29', crystax_version: 2
+  release version: '8.29', crystax_version: 3
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin', 'libexec'],
                 gen_android_mk:      false
+
+
+  SYMLINKS_SCRIPT = 'coreutils-create-symlinks.sh'
 
   def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, _target_dep_dirs, _options)
     install_dir = install_dir_for_abi(abi)
@@ -38,10 +41,27 @@ class Coreutils < Package
     system 'make', '-j', num_jobs
     system 'make', 'install'
 
-    # remove unneeded symlinks
-    FileUtils.cd(install_dir) do
-      files = Dir['bin/*'] - ['bin/coreutils']
-      FileUtils.rm files
+    FileUtils.cd("#{install_dir}/bin") do
+      symlinks = Dir['*'] - ['coreutils']
+      FileUtils.rm symlinks
+      write_create_symlinks_script symlinks
     end
+  end
+
+  def write_create_symlinks_script(symlinks)
+    File.open(SYMLINKS_SCRIPT, 'w') do |f|
+      f.puts '#!/bin/sh'
+      f.puts ''
+      f.puts 'cd $(dirname $0)'
+      f.puts ''
+      f.puts "single_file_binaries=\"#{symlinks.join(' ')}\""
+      f.puts ''
+      f.puts 'for i in $single_file_binaries; do'
+      f.puts 'ln -s coreutils $i;'
+      f.puts 'done'
+      f.puts ''
+      f.puts 'cd -'
+    end
+    FileUtils.chmod '+x', SYMLINKS_SCRIPT
   end
 end
