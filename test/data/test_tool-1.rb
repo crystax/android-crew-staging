7af40702ebee49f5f60d3973115500324bdc2fdc
Alexander Zhukov <zuav@crystax.net>
2018-04-10 19:32:32 +0300

    [test] Add test data mistakenly ignored
    Fix .gitignore to ignore only required files.

---
+++ test/data/test_tool-1.rb
@@ -0,0 +1,15 @@
+require_relative "#{Global::NDK_DIR}/../data/test_tool_methods.rb"
+
+class TestTool < Tool
+
+  include MultiVersion
+
+  desc 'Test Tool'
+  url ''
+
+  release version: '1.0.0', crystax_version: 1
+  release version: '2.0.0', crystax_version: 2
+  release version: '3.0.0', crystax_version: 1
+
+  include TestToolMethods
+end
