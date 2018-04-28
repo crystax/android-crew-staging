43b2959594323f5e40dcf16c9668b23b7434a881
Alexander Zhukov <zuav@crystax.net>
2018-04-29 00:30:59 +0300

    [erlang] Update to version 20.3.4

--- formula/packages/erlang.rb
+++ formula/packages/erlang.rb
@@ -4,7 +4,7 @@ class Erlang < Package
   homepage "https://www.erlang.org/"
   url "https://github.com/erlang/otp/archive/OTP-${version}.tar.gz"
 
-  release version: '20.0.2', crystax_version: 4
+  release version: '20.3.4', crystax_version: 1
 
   depends_on 'ncurses'
   depends_on 'openssl'
