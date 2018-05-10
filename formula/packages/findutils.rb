6d056b2019c24b527751d11f60780cfb1abdf715
Alexander Zhukov <zuav@crystax.net>
2018-05-10 14:59:10 +0300

    [findutil] Use Package::set_pthread_in_use_detection_hard method

--- formula/packages/findutils.rb
+++ formula/packages/findutils.rb
@@ -4,7 +4,7 @@ class Findutils < Package
   homepage 'https://www.gnu.org/software/findutils/'
   url 'https://ftp.gnu.org/pub/gnu/findutils/findutils-${version}.tar.gz'
 
-  release version: '4.6.0', crystax_version: 2
+  release version: '4.6.0', crystax_version: 3
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin', 'libexec'],
@@ -22,15 +22,7 @@ class Findutils < Package
 
     system './configure', *args
 
-    # configure desides to use pthread_cancel to check whether pthread is in use
-    # since we do not have pthread_cancel (at least right now) we'll handle the issue by editing config.h
-    replace_lines_in_file('config.h') do |line|
-      if line == '/* #undef PTHREAD_IN_USE_DETECTION_HARD */'
-        '#define PTHREAD_IN_USE_DETECTION_HARD 1'
-      else
-        line
-      end
-    end
+    set_pthread_in_use_detection_hard 'config.h'
 
     system 'make', '-j', num_jobs
     system 'make', 'install'
