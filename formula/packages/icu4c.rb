cbe662c60b7d67332a510156422d769c038cd11b
Alexander Zhukov <zuav@crystax.net>
2018-06-07 12:44:39 +0300

    [icu4c] Update to version 61.1
    Also build on ubuntu 18.04.

--- formula/packages/icu4c.rb
+++ formula/packages/icu4c.rb
@@ -4,10 +4,11 @@ class Icu4c < Package
   homepage "http://site.icu-project.org/"
   url "https://ssl.icu-project.org/files/icu4c/${version}/icu4c-${block}-src.tgz" do |r| r.version.gsub('.', '_') end
 
-  release version: '58.2', crystax_version: 3
+  #release version: '58.2', crystax_version: 3
+  release version: '61.1', crystax_version: 1
 
-  # this libs were in 57.1: 'libicule', 'libiculx'
-  build_libs 'libicudata', 'libicui18n', 'libicuio', 'libicutest', 'libicutu', 'libicuuc'
+  # this libs were in 57.1: 'libicule'
+  build_libs 'libicudata', 'libicui18n', 'libicuio', 'libicutest', 'libicutu', 'libicuuc', 'libiculx'
   build_copy 'license.html'
   build_options use_cxx: true
 
@@ -19,7 +20,9 @@ class Icu4c < Package
 
     Build.gen_host_compiler_wrapper "#{build_dir}/gcc", 'gcc', '-m32'
     Build.gen_host_compiler_wrapper "#{build_dir}/g++", 'g++', '-m32'
+
     build_env['PATH'] = "#{build_dir}:#{ENV['PATH']}"
+    build_env['ICULEHB_LIBS']   = ' '
 
     FileUtils.cd(build_dir) do
       system './source/runConfigureICU', icu_host_platform
@@ -45,17 +48,16 @@ class Icu4c < Package
     build_env['CFLAGS'] << ' -mabi=32 -mips32'   if abi == 'mips'
     build_env['CFLAGS'] << ' -mabi=64 -mips64r6' if abi == 'mips64'
 
+    build_env['ICULEHB_LIBS']   = ' '
+
     system './source/configure', *args
     system 'make', '-j', num_jobs
     system 'make', 'install'
 
-    # clean lib dir before packaging
+    clean_install_dir abi, :lib
     FileUtils.cd("#{install_dir}/lib") do
-      FileUtils.rm_rf ['pkgconfig', 'icu']
-      build_libs.each do |f|
-        FileUtils.rm ["#{f}.so", "#{f}.so.#{major_ver(release)}"]
-        FileUtils.mv "#{f}.so.#{release.version}", "#{f}.so"
-      end
+      FileUtils.rm_rf 'icu'
+      build_libs.each { |f| FileUtils.mv "#{f}.so.#{release.version}", "#{f}.so" }
     end
   end
 
@@ -72,13 +74,18 @@ class Icu4c < Package
 
   def sonames_translation_table(release)
     v = major_ver(release)
-    { "libicudata.so.#{v}" => 'libicudata',
-      "libicui18n.so.#{v}" => 'libicui18n',
-      "libicuio.so.#{v}"   => 'libicuio',
-      "libicutest.so.#{v}" => 'libicutest',
-      "libicutu.so.#{v}"   => 'libicutu',
-      "libicuuc.so.#{v}"   => 'libicuuc'
-    }
+    table = {}
+    build_libs.each { |l| table["#{l}.so.#{v}"] = l }
+
+    # { "libicudata.so.#{v}" => 'libicudata',
+    #   "libicui18n.so.#{v}" => 'libicui18n',
+    #   "libicuio.so.#{v}"   => 'libicuio',
+    #   "libicutest.so.#{v}" => 'libicutest',
+    #   "libicutu.so.#{v}"   => 'libicutu',
+    #   "libicuuc.so.#{v}"   => 'libicuuc'
+    # }
+
+    table
   end
 
   def major_ver(release)
