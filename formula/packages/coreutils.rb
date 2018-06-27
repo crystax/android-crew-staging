08360ce3270b67a388acd26c76899d5d8b772fb2
Alexander Zhukov <zuav@crystax.net>
2018-06-27 16:40:25 +0300

    [coreutils] Put coreutils binaries in /bin, not in /usr/bin

--- formula/packages/coreutils.rb
+++ formula/packages/coreutils.rb
@@ -4,7 +4,9 @@ class Coreutils < Package
   homepage "https://www.gnu.org/software/coreutils"
   url "http://ftpmirror.gnu.org/coreutils/coreutils-${version}.tar.xz"
 
-  release version: '8.29', crystax_version: 4
+  release version: '8.29', crystax_version: 5
+
+  package_info root_dir: ['bin']
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin', 'libexec'],
