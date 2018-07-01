98059b26871e0f19e09811fa84db048a7d8cc8b6
Dmitry Moskalchuk <dm@crystax.net>
2018-07-01 03:46:18 +0300

    [rsync] Add formula
    Signed-off-by: Dmitry Moskalchuk <dm@crystax.net>

---
+++ formula/packages/rsync.rb
@@ -0,0 +1,27 @@
+class Rsync < Package
+
+  desc "A fast, versatile, remote (and local) file-copying tool"
+  homepage "https://rsync.samba.org/"
+  url "https://download.samba.org/pub/rsync/src/rsync-${version}.tar.gz"
+
+  release version: '3.1.3', crystax_version: 1
+
+  build_copy 'COPYING'
+  build_options copy_installed_dirs: ['bin']
+
+  def build_for_abi(abi, _toolchain,  release, _host_dep_dirs, _target_dep_dirs, _options)
+    install_dir = install_dir_for_abi(abi)
+    src_dir = source_directory(release)
+
+    args = %W[ --prefix=#{install_dir}
+               --host=#{host_for_abi(abi)}
+               --with-included-popt
+             ]
+
+    system "#{src_dir}/configure", *args
+    system 'make', '-j', num_jobs
+    system 'make', 'install'
+
+    clean_install_dir abi, :lib
+  end
+end
