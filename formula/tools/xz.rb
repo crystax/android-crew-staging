6c6c788b62752212e028a81c8f9c19d8bffb23cb
Alexander Zhukov <zuav@crystax.net>
2018-07-04 16:14:45 +0300

    [xz] Made Utility out of xz and install bin/xz to use with our untar

--- formula/tools/xz.rb
+++ formula/tools/xz.rb
@@ -1,10 +1,10 @@
-class Xz < BuildDependency
+class Xz < Utility
 
   desc "General-purpose data compression with high compression ratio"
   homepage "http://tukaani.org/xz/"
   url "http://tukaani.org/xz/xz-${version}.tar.xz"
 
-  release '5.2.3', crystax: 3
+  release '5.2.4'
 
   def build_for_platform(platform, release, options, _host_dep_dirs, _target_dep_dirs)
     install_dir = install_dir_for_platform(platform.name, release)
@@ -27,7 +27,11 @@ class Xz < BuildDependency
     system 'make', 'check' if options.check? platform
     system 'make', 'install'
 
-    # remove unneeded files before packaging
-    FileUtils.cd(install_dir) { FileUtils.rm_rf ['bin/unxz', 'bin/xzcat', 'lib/liblzma.la', 'lib/pkgconfig', 'share'] }
+    clean_install_dir platform.name, release, :lib, :share
+    FileUtils.cd(install_dir) { FileUtils.rm_rf ['bin/unxz', 'bin/xzcat'] }
+  end
+
+  def split_file_list(list, platform_name)
+    split_file_list_by_static_libs_and_includes(list, platform_name)
   end
 end
