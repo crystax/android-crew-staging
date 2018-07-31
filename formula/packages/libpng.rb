96bdd10c97485ee837dffe569e8db4aa5c7796a7
Alexander Zhukov <zuav@crystax.net>
2018-07-31 12:46:27 +0300

    [libpng] Bump version, update for DSL and build api changes

--- formula/packages/libpng.rb
+++ formula/packages/libpng.rb
@@ -5,15 +5,16 @@ class Libpng < Package
   url "http://sourceforge.net/projects/libpng/files/libpng16/${version}/libpng-${version}.tar.xz"
   url "http://sourceforge.net/projects/libpng/files/libpng16/older-releases/${version}/libpng-${version}.tar.xz"
 
-  release '1.6.29', crystax: 3
+  release '1.6.35'
 
   build_copy 'LICENSE'
   build_options export_ldlibs: '-lz'
 
-  def build_for_abi(abi, _toolchain, release, _host_dep_dirs, _target_dep_dirs, _options)
+  def build_for_abi(abi, _toolchain, release, _options)
     install_dir = install_dir_for_abi(abi)
     args =  [ "--prefix=#{install_dir}",
               "--host=#{host_for_abi(abi)}",
+              "--disable-silent-rules",
               "--enable-shared",
               "--enable-static",
               "--disable-werror", # because of _POSIX_SOURCE redefinition
@@ -22,19 +23,14 @@ class Libpng < Package
             ]
     args << '--enable-arm-neon=api' if abi == 'armeabi-v7a' or abi == 'armeabi-v7a-hard'
 
-    system './configure', *args
-    system 'make', '-j', num_jobs
-    system 'make', 'install'
+    configure *args
+    make
+    make 'install'
 
-    # clean lib dir before packaging
+    clean_install_dir abi
     FileUtils.cd("#{install_dir}/lib") do
-      FileUtils.rm_rf 'pkgconfig'
-      FileUtils.rm Dir['*.la']
       vs = v2d(release)
-      ['a', 'so'].each do |ext|
-        FileUtils.rm "libpng.#{ext}"
-        FileUtils.mv "libpng#{vs}.#{ext}", "libpng.#{ext}"
-      end
+      ['a', 'so'].each { |ext| FileUtils.mv "libpng#{vs}.#{ext}", "libpng.#{ext}" }
     end
   end
 
