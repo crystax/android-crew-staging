eb9084e12d9924ba6a564a385f81b04d54872e44
Alexander Zhukov <zuav@crystax.net>
2018-07-31 12:46:01 +0300

    [libjpeg-turbo] Bump version, update for DSL and build api changes

--- formula/packages/libjpeg_turbo.rb
+++ formula/packages/libjpeg_turbo.rb
@@ -5,16 +5,17 @@ class LibjpegTurbo < Package
   homepage "http://www.libjpeg-turbo.org/"
   url "https://downloads.sourceforge.net/project/libjpeg-turbo/${version}/libjpeg-turbo-${version}.tar.gz"
 
-  release '1.4.2', crystax: 3
+  release '1.5.3'
 
+  build_copy 'LICENSE.md'
   build_libs 'libturbojpeg', 'libjpeg'
-  build_copy 'LICENSE.txt'
+
   build_options sysroot_in_cflags: false
 
-  def build_for_abi(abi, _toolchain, _release, _host_dep_dirs, _target_dep_dirs, _options)
-    install_dir = install_dir_for_abi(abi)
+  def build_for_abi(abi, _toolchain, _release, _options)
     args =  [ "--prefix=#{install_dir_for_abi(abi)}",
               "--host=#{host_for_abi(abi)}",
+              "--disable-silent-rules",
               "--enable-shared",
               "--enable-static",
               "--with-pic",
@@ -22,11 +23,10 @@ class LibjpegTurbo < Package
             ]
     args << '--without-simd' if abi == 'mips'
 
-    system './configure', *args
-    system 'make', '-j', num_jobs, 'V=1'
-    system 'make', 'install'
+    configure *args
+    make
+    make 'install'
 
-    # remove unneeded files
-    FileUtils.rm Dir["#{install_dir}/lib/*.la"]
+    clean_install_dir abi
   end
 end
