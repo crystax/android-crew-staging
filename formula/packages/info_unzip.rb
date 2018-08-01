30e93965e4a7d0ae8daabd59a4f03b0628c9fdf8
Alexander Zhukov <zuav@crystax.net>
2018-08-01 21:43:50 +0300

    [inf-zip,unzip] Update for DSL and build api changes

--- formula/packages/info_unzip.rb
+++ formula/packages/info_unzip.rb
@@ -5,19 +5,20 @@ class InfoUnzip < Package
   homepage 'http://www.info-zip.org/UnZip.html'
   url 'https://sourceforge.net/projects/infozip/files/UnZip%206.x%20%28latest%29/UnZip%20${version}/unzip${block}.tar.gz' do |r| r.version.split('.').first(2).join end
 
-  release '6.0', crystax: 2
+  release '6.0', crystax: 3
 
   build_copy 'LICENSE'
-  build_options cflags_in_c_wrapper:  true,
+  build_options build_outside_source_tree: false,
+                cflags_in_c_wrapper:  true,
                 ldflags_in_c_wrapper: true,
                 copy_installed_dirs:  ['bin'],
                 gen_android_mk:       false
 
 
-  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, _target_dep_dirs, _options)
+  def build_for_abi(abi, _toolchain,  _release, _options)
     build_env['PATH'] = "#{Dir.pwd}:#{ENV['PATH']}"
 
-    system "make -j #{num_jobs} -f unix/Makefile generic"
+    make '-f', 'unix/Makefile', 'generic'
 
     install_bin_dir = "#{install_dir_for_abi(abi)}/bin"
     FileUtils.mkdir_p install_bin_dir
