65792ea41b17856a1a36b3acb640e9e6b5674574
Alexander Zhukov <zuav@crystax.net>
2018-08-01 14:57:15 +0300

    [cpulimit] Update for DSL and build api changes

--- formula/packages/cpulimit.rb
+++ formula/packages/cpulimit.rb
@@ -4,15 +4,16 @@ class Cpulimit < Package
   homepage "https://github.com/opsengine/cpulimit"
   url "https://github.com/opsengine/cpulimit/archive/v${version}.tar.gz"
 
-  release '0.2', crystax: 2
+  release '0.2', crystax: 3
 
   build_copy 'LICENSE'
-  build_options ldflags_in_c_wrapper: true,
+  build_options build_outside_source_tree: false,
+                ldflags_in_c_wrapper: true,
                 copy_installed_dirs:  ['bin'],
                 gen_android_mk:       false
 
 
-  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, _target_dep_dirs, _options)
+  def build_for_abi(abi, _toolchain,  _release, _options)
     system 'make', '-j', num_jobs
 
     bin_dir = "#{install_dir_for_abi(abi)}/bin"
