29f58c48f3c734d8bb904d7bca996da31565f625
Alexander Zhukov <zuav@crystax.net>
2018-08-01 20:53:26 +0300

    [gzip] Bump version, update for DSL and build api changes

--- formula/packages/gzip.rb
+++ formula/packages/gzip.rb
@@ -4,21 +4,22 @@ class Gzip < Package
   homepage 'https://www.gnu.org/software/gzip/'
   url 'https://ftp.gnu.org/gnu/gzip/gzip-${version}.tar.gz'
 
-  release '1.2.4', crystax: 2
+  release '1.9'
 
   build_copy 'COPYING'
   build_options ldflags_in_c_wrapper: true,
                 copy_installed_dirs:  ['bin'],
                 gen_android_mk:       false
 
-  def build_for_abi(abi, _toolchain, _release, _host_dep_dirs, target_dep_dirs, _options)
-    install_dir = install_dir_for_abi(abi)
-    FileUtils.mkdir_p install_dir
+  def build_for_abi(abi, _toolchain, _release, _options)
+    args = ["--prefix=#{install_dir_for_abi(abi)}",
+            "--host=#{host_for_abi(abi)}",
+            "--disable-silent-rules",
+            "--disable-rpath"
+           ]
 
-    args =  ["--prefix=#{install_dir}", "--host=#{host_for_abi(abi)}"]
-
-    system './configure', *args
-    system 'make', '-j', num_jobs
-    system 'make', 'install'
+    configure *args
+    make
+    make 'install'
   end
 end
