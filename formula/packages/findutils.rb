7c5efc017e0eaa06aeac65fe4c6613f9fa5c65a6
Alexander Zhukov <zuav@crystax.net>
2018-08-01 16:09:23 +0300

    [findutils] Update for DSL and build api changes

--- formula/packages/findutils.rb
+++ formula/packages/findutils.rb
@@ -4,27 +4,26 @@ class Findutils < Package
   homepage 'https://www.gnu.org/software/findutils/'
   url 'https://ftp.gnu.org/pub/gnu/findutils/findutils-${version}.tar.gz'
 
-  release '4.6.0', crystax: 3
+  release '4.6.0', crystax: 4
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin', 'libexec'],
                 gen_android_mk: false
 
 
-  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, _target_dep_dirs, _options)
-    install_dir = install_dir_for_abi(abi)
-    args = ["--prefix=#{install_dir}",
+  def build_for_abi(abi, _toolchain,  _release, _options)
+    args = ["--prefix=#{install_dir_for_abi(abi)}",
             "--host=#{host_for_abi(abi)}",
             "--disable-silent-rules",
             "--disable-rpath",
             "--disable-nls"
            ]
 
-    system './configure', *args
+    configure *args
 
     set_pthread_in_use_detection_hard 'config.h'
 
-    system 'make', '-j', num_jobs
-    system 'make', 'install'
+    make
+    make 'install'
   end
 end
