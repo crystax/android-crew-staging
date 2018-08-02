5dae8a090c1e9c240680bc9f14885f659d29f135
Alexander Zhukov <zuav@crystax.net>
2018-08-02 23:34:49 +0300

    [htop] Update for DSL and build api changes
    Also change url to get tarball with ready configure script.

--- formula/packages/htop.rb
+++ formula/packages/htop.rb
@@ -2,35 +2,30 @@ class Htop < Package
 
   desc 'htop is an interactive process viewer for Unix systems'
   homepage 'https://hisham.hm/htop/'
-  url 'https://github.com/hishamhm/htop/archive/${version}.tar.gz'
+  url 'https://hisham.hm/htop/releases/${version}/htop-${version}.tar.gz'
 
-  release '2.2.0'
+  release '2.2.0', crystax: 2
 
   depends_on 'ncurses'
 
   build_copy 'COPYING'
-  build_options copy_installed_dirs:  ['bin'],
+  build_options add_deps_to_cflags: true,
+                add_deps_to_ldflags: true,
                 ldflags_in_c_wrapper: true,
+                copy_installed_dirs:  ['bin'],
                 gen_android_mk:       false
 
 
-  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, target_dep_dirs, _options)
-    install_dir = install_dir_for_abi(abi)
-    ncurses_dir = target_dep_dirs['ncurses']
-
-    build_env['CFLAGS']  += " -I#{ncurses_dir}/include -L#{ncurses_dir}/libs/#{abi}"
-    build_env['LIBS']     = '-lncursesw'
-
-    args =  [ "--prefix=#{install_dir}",
+  def build_for_abi(abi, _toolchain,  _release, _options)
+    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
               "--host=#{host_for_abi(abi)}",
               "--disable-silent-rules",
 	      "--enable-unicode",
               "--enable-linux-affinity"
             ]
 
-    system './autogen.sh'
-    system './configure', *args
-    system 'make', '-j', num_jobs
-    system 'make', 'install'
+    configure *args
+    make
+    make 'install'
   end
 end
