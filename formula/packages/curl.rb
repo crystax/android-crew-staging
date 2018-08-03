fbcbe134908d7ecdb8820d4b3dd23f7fa59544a7
Alexander Zhukov <zuav@crystax.net>
2018-08-03 18:46:25 +0300

    [target/curl] Bump version, update for DSL and build api changes

--- formula/packages/curl.rb
+++ formula/packages/curl.rb
@@ -4,50 +4,33 @@ class Curl < Package
   homepage 'http://curl.haxx.se/'
   url 'https://curl.haxx.se/download/curl-${version}.tar.bz2'
 
-  release '7.58.0', crystax: 4
+  #release '7.58.0', crystax: 4
+  release '7.61.0'
 
   depends_on 'openssl'
   depends_on 'libssh2'
 
-  build_options copy_installed_dirs: ['bin', 'include', 'lib']
-
   build_copy 'COPYING'
+  build_options add_deps_to_cflags: true,
+                add_deps_to_ldflags: true,
+                copy_installed_dirs: ['bin', 'include', 'lib']
 
-  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, target_dep_dirs, _options)
-    install_dir = install_dir_for_abi(abi)
-    openssl_dir = target_dep_dirs['openssl']
-    libssh2_dir = target_dep_dirs['libssh2']
-
-
-    args =  [ "--prefix=#{install_dir}",
+  def build_for_abi(abi, _toolchain,  _release, _options)
+    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
               "--host=#{host_for_abi(abi)}",
-              "--with-ssl=#{openssl_dir}",
+              "--with-ssl",
               "--with-libssh2",
               "--disable-nls",
              " --disable-silent-rules"
             ]
 
-    build_env['CFLAGS']  += " -I#{openssl_dir}/include -I#{libssh2_dir}/include"
-    build_env['LDFLAGS'] += " -L#{openssl_dir}/libs/#{abi} -L#{libssh2_dir}/libs/#{abi}"
+    # for some reason libtool for some abis does not handle dependency libs
+    build_env['LDFLAGS'] += ' -lssh2 -lssl -lcrypto -lz' if ['mips', 'arm64-v8a', 'mips64'].include? abi
 
-    system './configure', *args
+    configure *args
+    make '-j', num_jobs
+    make 'install'
 
-    fix_makefile if ['mips', 'arm64-v8a', 'mips64'].include? abi
-
-    system 'make', '-j', num_jobs
-    system 'make', 'install'
-
-    clean_install_dir abi, :lib
-  end
-
-  # for some reason libtool for some abis does not handle dependency libs
-  def fix_makefile
-    replace_lines_in_file('src/Makefile') do |line|
-      if not line =~ /^LDFLAGS =[ \t]*/
-        line
-      else
-        line.gsub('LDFLAGS =', 'LDFLAGS = -lssh2 -lssl -lcrypto -lz ')
-      end
-    end
+    clean_install_dir abi
   end
 end
