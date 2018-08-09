499fdd00197589b9571eb5efb05f587282dc0309
Alexander Zhukov <zuav@crystax.net>
2018-08-09 21:44:59 +0300

    [target/wget] Update for DSL and build api changes
    Also build with libpcre-old since wget does not support new version of libpcre.

--- formula/packages/wget.rb
+++ formula/packages/wget.rb
@@ -4,33 +4,19 @@ class Wget < Package
   homepage "https://www.gnu.org/software/wget/"
   url "http://ftp.gnu.org/gnu/wget/wget-${version}.tar.gz"
 
-  release '1.19.5', crystax: 2
+  release '1.19.5', crystax: 3
 
   depends_on 'openssl'
   depends_on 'libunistring'
   depends_on 'libidn2'
-  depends_on 'libpcre'
+  depends_on 'libpcre-old'
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin', 'etc'],
                 gen_android_mk:      false
 
-  def build_for_abi(abi, _toolchain,  _release, _host_dep_dirs, target_dep_dirs, _options)
-    install_dir = install_dir_for_abi(abi)
-    openssl_dir = target_dep_dirs['openssl']
-    libunistring_dir = target_dep_dirs['libunistring']
-    libidn2_dir = target_dep_dirs['libidn2']
-    libpcre_dir = target_dep_dirs['libpcre']
-
-    build_env['OPENSSL_CFLAGS'] = "-I#{openssl_dir}/include"
-    build_env['OPENSSL_LIBS']   = "-L#{openssl_dir}/libs/#{abi} -lssl -lcrypto"
-    build_env['PCRE_CFLAGS']    = "-I#{libpcre_dir}/include"
-    build_env['PCRE_LIBS']      = "-L#{libpcre_dir}/libs/#{abi} -lpcre2-8"
-
-    build_env['CFLAGS']  += " -I#{openssl_dir}/include -I#{libunistring_dir}/include -I#{libidn2_dir}/include -I#{libpcre_dir}/include"
-    build_env['LDFLAGS'] += " -L#{openssl_dir}/libs/#{abi} -L#{libunistring_dir}/libs/#{abi} -L#{libidn2_dir}/libs/#{abi} -L#{libpcre_dir}/libs/#{abi}"
-
-    args =  [ "--prefix=#{install_dir}",
+  def build_for_abi(abi, _toolchain,  _release, _options)
+    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
               "--host=#{host_for_abi(abi)}",
               "--disable-silent-rules",
               "--disable-nls",
@@ -40,8 +26,14 @@ class Wget < Package
               "--without-cares"
             ]
 
-    system './configure', *args
-    system 'make', '-j', num_jobs
-    system 'make', 'install'
+    build_env['OPENSSL_CFLAGS'] = "-I#{target_dep_include_dir('openssl')}"
+    build_env['OPENSSL_LIBS']   = "-L#{target_dep_lib_dir('openssl', abi)} -lssl -lcrypto"
+
+    build_env['CFLAGS']  += " -I#{target_dep_include_dir('libidn2')}"
+    build_env['LDFLAGS'] += " -L#{target_dep_lib_dir('libidn2', abi)} -lunistring"
+
+    configure *args
+    make
+    make 'install'
   end
 end
