96fa7baea2630a34282523c6b4b982a94c85e5ca
Alexander Zhukov <zuav@crystax.net>
2018-08-09 20:46:29 +0300

    [target/libpcre] Copy LICENCE file

--- formula/packages/libpcre.rb
+++ formula/packages/libpcre.rb
@@ -7,7 +7,7 @@ class Libpcre < Package
   release '10.31'
 
   build_libs 'libpcre2-8', 'libpcre2-posix'
-  build_copy 'COPYING'
+  build_copy 'LICENCE'
   build_options copy_installed_dirs: ['bin', 'include', 'lib']
 
   def build_for_abi(abi, _toolchain, release, _options)
