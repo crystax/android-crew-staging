90c1433e4d4ca3e95dee3cdfdf02854d2692c985
Alexander Zhukov <zuav@crystax.net>
2018-08-21 19:05:16 +0300

    [tests] Add tests for depends-on command

---
+++ test/cmd_depends_on_spec.rb
@@ -0,0 +1,66 @@
+# must be first file included
+require_relative 'spec_helper.rb'
+
+require_relative '../library/global.rb'
+
+describe "crew depends-on" do
+  before(:all) do
+    environment_init
+    ndk_init
+  end
+
+  before(:each) do
+    clean_hold
+    clean_cache
+    repository_init
+    repository_clone
+  end
+
+  context 'no arguments' do
+    it 'depends-on command outputs error message' do
+      crew 'depends-on'
+      expect(exitstatus).to_not be_zero
+      expect(err.strip).to match(/error: .*/)
+    end
+  end
+
+  context 'more than one argument' do
+    it 'depends-on command outputs error message' do
+      crew 'depends-on', 'libone', 'libtwo'
+      expect(exitstatus).to_not be_zero
+      expect(err.strip).to match(/error: .*/)
+    end
+  end
+
+  context "repository contains package formula with no dependencies" do
+    it "depends-on command outputs nothing" do
+      copy_packages_formulas 'libone.rb'
+      crew 'depends-on', 'libone'
+      expect(exitstatus).to be_zero
+      expect(out.strip).to eq('')
+    end
+  end
+
+  context "repository contains package two formulas, one depends on another" do
+
+    before do
+      copy_packages_formulas 'libone.rb', 'libtwo.rb'
+    end
+
+    context 'check formula that has no dependant formulas' do
+      it "depends-on command outputs nothing" do
+        crew 'depends-on', 'libtwo'
+        expect(exitstatus).to be_zero
+        expect(out.strip).to eq('')
+      end
+    end
+
+    context 'check formula that has dependant formulas' do
+      it "depends-on command outputs dependency fully qualified name" do
+        crew 'depends-on', 'libone'
+        expect(exitstatus).to be_zero
+        expect(out.strip).to eq('target/libtwo')
+      end
+    end
+  end
+end
