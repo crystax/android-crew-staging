00108adf7dfb768850d85897e387e9e50bbf3bb3
Alexander Zhukov <zuav@crystax.net>
2018-10-24 22:22:31 +0300

    [ruby] Bump version

--- formula/tools/ruby.rb
+++ formula/tools/ruby.rb
@@ -4,7 +4,7 @@ class Ruby < Utility
   homepage 'https://www.ruby-lang.org/'
   url 'https://cache.ruby-lang.org/pub/ruby/${block}/ruby-${version}.tar.gz' do |r| r.version.split('.').slice(0, 2).join('.') end
 
-  release '2.5.1', crystax: 2
+  release '2.5.3'
 
   depends_on 'zlib'
   depends_on 'openssl'
