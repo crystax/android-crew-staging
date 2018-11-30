09dd0e388d2fbd78434717c5f444e40fbabae86a
Alexander Zhukov <zuav@crystax.net>
2018-11-30 11:40:52 +0300

    [target/python] Bump crystax version

--- formula/packages/python.rb
+++ formula/packages/python.rb
@@ -4,8 +4,8 @@ class Python < Package
   homepage "https://www.python.org"
   url "https://www.python.org/ftp/python/${version}/Python-${version}.tgz"
 
-  release '2.7.11', crystax: 3
-  release '3.5.1',  crystax: 3
+  release '2.7.11', crystax: 4
+  release '3.5.1',  crystax: 4
 
   depends_on 'sqlite'
   depends_on 'openssl', version: /^1\.0/
