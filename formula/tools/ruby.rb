603388ccf779a25ee4fb0101ba0fa14e60f7db2e
Alexander Zhukov <zuav@crystax.net>
2018-12-14 17:57:12 +0300

    [host/ruby] Add postpone installation

--- formula/tools/ruby.rb
+++ formula/tools/ruby.rb
@@ -4,12 +4,14 @@ class Ruby < Library
   homepage 'https://www.ruby-lang.org/'
   url 'https://cache.ruby-lang.org/pub/ruby/${block}/ruby-${version}.tar.gz' do |r| r.version.split('.').slice(0, 2).join('.') end
 
-  release '2.5.3', crystax: 2
+  release '2.5.3', crystax: 3
 
   depends_on 'zlib'
   depends_on 'openssl'
   depends_on 'libssh2'
   depends_on 'libgit2'
+
+  postpone_install true
 
   GEMS = {'rspec' => '3.7.0', 'minitar' => '0.6.1'}
 
