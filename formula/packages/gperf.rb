028319da565689f19a7eadde793eed9dd5b9fbb6
Alexander Zhukov <zuav@crystax.net>
2019-02-01 22:46:41 +0300

    [target/gperf] Rebuild with default configure args

--- formula/packages/gperf.rb
+++ formula/packages/gperf.rb
@@ -3,22 +3,17 @@ class Gperf < Package
   name 'gperf'
   desc "GNU gperf is a perfect hash function generator"
   homepage "https://www.gnu.org/software/gperf/"
-  url "http://ftp.gnu.org/pub/gnu/gperf/gperf-${version}.tar.gz"
+  url "https://ftp.gnu.org/pub/gnu/gperf/gperf-${version}.tar.gz"
 
-  release '3.1', crystax: 3
+  release '3.1', crystax: 4
 
   build_copy 'COPYING'
   build_options use_cxx: true,
                 copy_installed_dirs: ['bin']
 
   def build_for_abi(abi, _toolchain, _release, _options)
-    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
-              "--host=#{host_for_abi(abi)}"
-            ]
-
     build_env['CXXFLAGS'] += ' -lgnustl_shared'
-
-    configure *args
+    configure
     make
     make 'install'
   end
