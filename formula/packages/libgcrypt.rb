41c079f317f642a838d680e526440ea0ad2fbb90
Alexander Zhukov <zuav@crystax.net>
2019-02-01 22:52:16 +0300

    [target/libgcrypt] Bump version, rebuild with default configure args

--- formula/packages/libgcrypt.rb
+++ formula/packages/libgcrypt.rb
@@ -4,16 +4,14 @@ class Libgcrypt < Package
   homepage "https://www.gnupg.org/software/libgcrypt/"
   url "https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-${version}.tar.bz2"
 
-  release '1.8.3', crystax: 2
+  release '1.8.4'
 
   depends_on 'libgpg-error'
 
   build_copy 'COPYING','COPYING.LIB'
 
   def build_for_abi(abi, _toolchain,  _release, _options)
-    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
-              "--host=#{host_for_abi(abi)}",
-              "--disable-silent-rules",
+    args =  [ "--disable-silent-rules",
               "--enable-shared",
               "--enable-static",
               "--disable-doc",
@@ -22,9 +20,11 @@ class Libgcrypt < Package
             ]
     args << '--disable-asm' if ['x86', 'x86_64'].include? Build.arch_for_abi(abi).name
 
-    build_env['GPG_ERROR_CFLAGS'] = "-I#{target_dep_include_dir('libgpg-error')}"
-    build_env['GPG_ERROR_LIBS']   = "-L#{target_dep_lib_dir('libgpg-error', abi)} -lgpg-error"
-    build_env['LDFLAGS']         += ' -lgpg-error'
+    build_env['GPG_ERROR_CFLAGS']    = "-I#{target_dep_include_dir('libgpg-error')}"
+    build_env['GPG_ERROR_LIBS']      = "-L#{target_dep_lib_dir('libgpg-error', abi)} -lgpg-error"
+    build_env['GPG_ERROR_MT_CFLAGS'] = build_env['GPG_ERROR_CFLAGS']
+    build_env['GPG_ERROR_MT_LIBS']   = "#{build_env['GPG_ERROR_LIBS']} -lpthread"
+    build_env['LDFLAGS']            += build_env['GPG_ERROR_LIBS']
 
     configure *args
     make
