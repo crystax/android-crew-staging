eaf27c2a3bf8aa3f6fce3179059964018e12b73a
Alexander Zhukov <zuav@crystax.net>
2019-02-01 22:44:53 +0300

    [target/gnu-grep] Bump version, rebuild with default configure args

--- formula/packages/gnu_grep.rb
+++ formula/packages/gnu_grep.rb
@@ -5,19 +5,14 @@ class GnuGrep < Package
   homepage "https://www.gnu.org/software/grep/"
   url "https://ftp.gnu.org/gnu/grep/grep-${version}.tar.xz"
 
-  release '3.1', crystax: 3
+  release '3.3'
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin'],
                 gen_android_mk:      false
 
   def build_for_abi(abi, _toolchain, _release, _options)
-    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
-              "--host=#{host_for_abi(abi)}",
-              "--disable-silent-rules",
-              "--disable-nls"
-            ]
-
+    args =  [ "--disable-silent-rules", "--disable-nls" ]
     configure *args
     make
     make 'install'
