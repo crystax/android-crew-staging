1c49d2cbf26b2b4cac4af8e90f69457d0c1143dc
Alexander Zhukov <zuav@crystax.net>
2019-02-01 22:53:14 +0300

    [target/libksba] Rebuild with default configure args

--- formula/packages/libksba.rb
+++ formula/packages/libksba.rb
@@ -4,7 +4,7 @@ class Libksba < Package
   homepage "https://github.com/gpg/libksba"
   url "https://www.gnupg.org/ftp/gcrypt/libksba/libksba-${version}.tar.bz2"
 
-  release '1.3.5', crystax: 2
+  release '1.3.5', crystax: 3
 
   depends_on 'libgpg-error'
 
@@ -13,9 +13,7 @@ class Libksba < Package
                 add_deps_to_ldflags: true
 
   def build_for_abi(abi, _toolchain,  _release, _options)
-    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
-              "--host=#{host_for_abi(abi)}",
-              "--disable-silent-rules",
+    args =  [ "--disable-silent-rules",
               "--enable-shared",
               "--enable-static",
               "--disable-doc",
