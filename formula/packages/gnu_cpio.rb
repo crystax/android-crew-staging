40b2c304b54332bf8c8b1aa6220840c07af88d44
Alexander Zhukov <zuav@crystax.net>
2019-02-01 22:44:32 +0300

    [target/gnu-cpio] Rebuild with default configure args

--- formula/packages/gnu_cpio.rb
+++ formula/packages/gnu_cpio.rb
@@ -3,9 +3,9 @@ class GnuCpio < Package
   name 'gnu-cpio'
   desc "GNU cpio copies files into or out of a cpio or tar archive. The archive can be another file on the disk, a magnetic tape, or a pipe"
   homepage "https://www.gnu.org/software/cpio/"
-  url "http://ftp.gnu.org/gnu/cpio/cpio-${version}.tar.bz2"
+  url "https://ftp.gnu.org/gnu/cpio/cpio-${version}.tar.bz2"
 
-  release '2.12', crystax: 3
+  release '2.12', crystax: 4
 
   build_options copy_installed_dirs: ['bin', 'lib', 'libexec'],
                 gen_android_mk:      false
@@ -13,12 +13,7 @@ class GnuCpio < Package
   build_copy 'COPYING'
 
   def build_for_abi(abi, _toolchain,  _release, _options)
-    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
-              "--host=#{host_for_abi(abi)}",
-              "--disable-nls",
-             " --disable-silent-rules"
-            ]
-
+    args =  [ "--disable-nls", " --disable-silent-rules" ]
     configure *args
     make
     make 'install'
