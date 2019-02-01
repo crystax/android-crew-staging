d565808a3a42579f0bc1ffdc039a122b60329a78
Alexander Zhukov <zuav@crystax.net>
2019-02-01 22:49:20 +0300

    [target/libffi] Rebuild with default configure args
    Also change url to get tarball with ready configure script.

--- formula/packages/libffi.rb
+++ formula/packages/libffi.rb
@@ -2,24 +2,21 @@ class Libffi < Package
 
   desc 'A portable foreign-function interface library'
   homepage 'https://github.com/libffi/libffi'
-  url 'https://github.com/libffi/libffi/archive/v${version}.tar.gz'
+  url 'ftp://sourceware.org/pub/libffi/libffi-${version}.tar.gz'
 
-  release '3.2.1', crystax: 3
+  release '3.2.1', crystax: 4
 
   build_copy 'LICENSE'
 
   def build_for_abi(abi, _toolchain, release, _options)
     install_dir = install_dir_for_abi(abi)
-    args =  [ "--prefix=#{install_dir}",
-              "--host=#{host_for_abi(abi)}",
-              "--disable-silent-rules",
+    args =  [ "--disable-silent-rules",
               "--enable-shared",
               "--enable-static",
               "--with-sysroot",
               "--includedir=#{install_dir}/include"
             ]
 
-    FileUtils.cd(source_directory(release)) { system './autogen.sh' }
     configure *args
     make
     make 'install'
