9ab39eae8894ef48cf132687c9ec40fc89b4891e
Alexander Zhukov <zuav@crystax.net>
2019-02-01 22:45:40 +0300

    [target/gnu-tar] Bump version, rebuild with default configure args

--- formula/packages/gnu_tar.rb
+++ formula/packages/gnu_tar.rb
@@ -5,19 +5,14 @@ class GnuTar < Package
   homepage 'https://www.gnu.org/software/tar/'
   url 'https://ftp.gnu.org/gnu/tar/tar-${version}.tar.xz'
 
-  release '1.30', crystax: 3
+  release '1.31'
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin', 'libexec'],
                 gen_android_mk:      false
 
   def build_for_abi(abi, _toolchain, _release, _options)
-    args =  ["--prefix=#{install_dir_for_abi(abi)}",
-             "--host=#{host_for_abi(abi)}",
-             "--disable-silent-rules",
-             "--disable-nls"
-            ]
-
+    args =  [ "--disable-silent-rules", "--disable-nls" ]
     configure *args
     make
     make 'install'
