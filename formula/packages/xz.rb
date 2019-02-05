41db56785cce7ee3294dea2e02c992e463eb3d5c
Alexander Zhukov <zuav@crystax.net>
2019-02-06 00:50:10 +0300

    [target/xz] Rebuild with default configure args

--- formula/packages/xz.rb
+++ formula/packages/xz.rb
@@ -1,19 +1,17 @@
 class Xz < Package
 
   desc "General-purpose data compression with high compression ratio"
-  homepage "http://tukaani.org/xz/"
-  url "http://tukaani.org/xz/xz-${version}.tar.xz"
+  homepage "https://tukaani.org/xz/"
+  url "https://tukaani.org/xz/xz-${version}.tar.xz"
 
-  release '5.2.4', crystax: 2
+  release '5.2.4', crystax: 3
 
   build_copy 'COPYING'
   build_options copy_installed_dirs: ['bin', 'include', 'lib']
   build_libs 'liblzma'
 
   def build_for_abi(abi, _toolchain,  _release, _options)
-    args =  [ "--prefix=#{install_dir_for_abi(abi)}",
-              "--host=#{host_for_abi(abi)}",
-              "--disable-silent-rules",
+    args =  [ "--disable-silent-rules",
               "--disable-nls",
               "--disable-doc",
               "--disable-lzma-links",
