d2749caeb019e28889c878e5d0b16d6af953aabf
Alexander Zhukov <zuav@crystax.net>
2019-02-14 00:05:24 +0300

    [target/bison] Build on darwin system

---
+++ formula/packages/bison.rb
@@ -0,0 +1,29 @@
+class Bison < Package
+
+  desc "Bison is a general-purpose parser generator"
+  homepage "https://www.gnu.org/software/bison/"
+  url "https://ftp.gnu.org/gnu/bison/bison-${version}.tar.xz"
+
+  release '3.3.2'
+
+  #depends_on 'readline'
+
+  build_copy 'COPYING'
+  build_options copy_installed_dirs: ['bin', 'lib', 'share'],
+                gen_android_mk:      false
+
+
+  def build_for_abi(abi, toolchain,  _release, _options)
+    args = [ '--disable-silent-rules',
+             '--disable-rpath',
+             '--disable-nls'
+           ]
+
+    configure *args
+    make
+    make 'install'
+
+    # remove unneeded files
+    FileUtils.cd("#{install_dir_for_abi(abi)}/share") { FileUtils.rm_r ['doc', 'info', 'man'] }
+  end
+end
