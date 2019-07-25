874628124179cc8c6c9236ff293bf6f2812f2717
Alexander Zhukov <zuav@crystax.net>
2019-07-25 21:32:41 +0300

    [target/curl] Bump version

--- formula/tools/curl.rb
+++ formula/tools/curl.rb
@@ -4,7 +4,7 @@ class Curl < Library
   homepage 'http://curl.haxx.se/'
   url 'https://curl.haxx.se/download/curl-${version}.tar.bz2'
 
-  release '7.65.0'
+  release '7.65.3'
 
   depends_on 'zlib'
   depends_on 'openssl'
